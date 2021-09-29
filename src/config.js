09ba0f0f4e83e7b9a831df05d20c2e669cc592c8
AppdevMACBook10 <appdevmacbook10@AppdevMACBook10s-MacBook-Pro.local>
2021-09-29 15:48:18 +0100

    added an endpoint for mailing Newcore Technologies

--- src/config.js
+++ src/config.js
@@ -4,6 +4,8 @@ module.exports = {
     secretKey: process.env.TOKEN_KEY,
     emailUser: process.env.EMAIL_USER,
     emailPass: process.env.EMAIL_PASS,
+    newcoreEmail: process.env.NEWCORE_EMAIL,
+    newcoreEmailPass: process.env.NEWCORE_EMAIL_PASS,
     cloudName: process.env.CLOUD_NAME,
     apiKey: process.env.API_KEY,
     apiSecret: process.env.API_SECRET,

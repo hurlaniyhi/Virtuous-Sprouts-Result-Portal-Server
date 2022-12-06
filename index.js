9ce2b0317f533f56eaccb3e7fa3b1b8c618b1c8e
unknown <olaniyi.jibola152@gmail.com>
2022-12-06 23:05:06 +0000

    make it better

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use(cors({
 
 app.use(bodyParser.json())
 app.get('/', (req, res) => {
-    res.send({message: 'Welcome back'}) 
+    res.send({message: 'Welcome home'}) 
 })
 app.use("/",authRoute)
 app.use("/",resultRoute)

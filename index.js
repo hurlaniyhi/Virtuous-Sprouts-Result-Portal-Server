14864a6499b3677077f9c4c2eb53ac3314358df5
Ridwan Kolawole <rkolawole@appzonegroup.com>
2022-12-07 13:20:46 +0100

    make it better

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use((req, res, next) => {
 })
 
 app.get('/', (req, res) => {
-    res.send({message: 'Welcome home'}) 
+    res.send({message: 'Welcome back to school'}) 
 })
 app.use("/",authRoute)
 app.use("/",resultRoute)
